f38aaa18c06b941e8c2d9ccdfa04726277396fa0
Kevin Petit <petitkevin20@gmail.com>
2021-03-14 21:17:33 -0400

    adding htttps transform

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('REDIRECT_HTTPS')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
+        // if (env('REDIRECT_HTTPS')) {
+        //     $this->app['request']->server->set('HTTPS', true);
+        // }
     }
 
     /**
@@ -24,10 +24,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if (env('REDIRECT_HTTPS')) {
-            $url->formatScheme('https://');
-        }
+        $this->app['request']->server->set('HTTPS', true);
     }
 }
